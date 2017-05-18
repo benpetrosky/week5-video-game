0d66d87c97f5a913c9736d62bb45eecc7f6dd58c
Aaron Nguyen <aaron3831@gmail.com>
2017-05-18 09:27:33 -0700

    merge into master

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 20170517182536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "friends_lists", force: :cascade do |t|
-    t.integer "friend_id"
-    t.integer "profile_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "opinions", force: :cascade do |t|
     t.integer "profile_id"
     t.integer "videogame_id"
